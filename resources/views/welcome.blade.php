846ffff2344620205607e0a6f15e25e74fe640cb
busseauc77 <busseauc@gmail.com>
2023-07-10 11:20:58 -0400

    vérification

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Laravel Livewire App</title>
+    @livewireStyles
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body class="bg-gray-100 h-screen flex items-center justify-center">
+   
+    @livewire('button', ['lable' => "click on me"])
+
+
+    @livewireScripts
+</body>
+</html>

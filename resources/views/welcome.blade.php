0b2607f5f0673e65727add1c5ac3be3e67a682c9
busseauc77 <busseauc@gmail.com>
2023-07-10 10:12:04 -0400

    test avec component Counter + ajout des icônes et fichier de test des icônes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,24 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Laravel Livewire App</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    @livewireStyles
 </head>
+
 <body class="bg-gray-100 h-screen flex items-center justify-center">
     <div class="container mx-auto bg-white p-8 rounded shadow-lg text-center">
+        <img class='object-contain h-24' src="/icons/VinoRedLight.svg" alt="Vino Logo">
         <div>
-            <h1 class="text-3xl font-bold mb-4">My Laravel Livewire App</h1>
-            
+            <h1 class="text-3xl font-bold mb-4">Mon application de gestion de celliers</h1>
+
             <div class="bg-gray-300 p-4 rounded">
-                <p class="text-purple-700">Welcome to my Livewire app! This is a simple example component.</p>
+                <p class="text-purple-700">Bienvenue! Je suis une composante.</p>
+                @livewire('counter')
             </div>
-            
+
             @livewire('test')
         </div>
     </div>
 
     @livewireScripts
 </body>
+
 </html>

12edbbfc6957e46e73c687b3035dabb0ccfdc487
maryline888 <maryline888@gmail.com>
2023-07-13 15:43:20 -0400

    modif on button componen

--- app/Http/Livewire/Button.php
+++ app/Http/Livewire/Button.php
@@ -6,20 +6,22 @@ use Livewire\Component;
 
 class Button extends Component
 {
-    public $lable;
+    public $label;
+    public $class;
 
-    public function mount($lable = 'Click')
+    public function mount($label = 'Submit', $class = 'btn btn-primary')
     {
-        $this->lable = $lable;
-    }
-
-    public function render()
-    {
-        return view('livewire.button', ['lable', $this->lable]);
+        $this->label = $label;
+        $this->class = $class;
     }
 
     public function handleClick()
     {
-        // here you can define what happens when the button is clicked
+        // coder la fonction handleClick
+    }
+
+    public function render()
+    {
+        return view('livewire.button');
     }
 }

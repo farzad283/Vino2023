b7028f1385475ca82ea43ec6dd1d24a26cff3bcc
busseauc77 <busseauc@gmail.com>
2023-07-15 13:55:25 -0400

    réglement du problème de dépendance entre SingleBottle & ManyBottles

--- resources/views/livewire/many-bottles.blade.php
+++ resources/views/livewire/many-bottles.blade.php
@@ -1,6 +1,6 @@
 <div class="grid grid-cols-2 gap-2 ">
         @foreach ($bottles as $bottle)
-            @livewire('single-bottle', ['bottle' => $bottle])
+            @livewire('single-bottle', ['bottleId' => $bottle->id])
         @endforeach
     <div class="col-span-2">{{ $bottles->links() }}</div>
 </div>

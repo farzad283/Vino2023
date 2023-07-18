15245c2769ad4e16e252982e80e323a334b0f2bb
maryline888 <maryline888@gmail.com>
2023-07-18 09:11:18 -0400

    modif header

--- resources/views/livewire/single-bottle.blade.php
+++ resources/views/livewire/single-bottle.blade.php
@@ -1,22 +1,10 @@
-<article class=" m-4 border-2 border-gold m-2 flex rounded-lg ">
-    <div class=" top-[-20px]  ">
-        <img src="{{ $bottle->image }}" alt="{{ $bottle->name }}" class="max-w-100 mt-2 ">
-    </div>
-    <div class="flex flex-col justify-end items-end ml-auto p-4">
-        <h1 class="font-bold text-right font-roboto">{{ $bottle->name }}</h1>
-
-        <p class="text-sm mt-2 mb-2">{{ $bottle->description }}</p>
-        <!-- et ainsi de suite pour les autres attributs de la bouteille -->
-        <a href="{{ $bottle->url }}" class="mb-2 underline "><span class="text-sm mt-2">Prix SAQ: {{ $bottle->price }} $</span></a>
-        <div class="flex mt-4 ">
-            <div class="flex items-center justify-center h-8 w-26 ">
-                <button id="decrement" class="w-8 h-8 p-2 text-xl font-bold text-gold flex items-center justify-center">-</button>
-                <span id="count" class="mx-4 text-gold">0</span>
-                <button id="increment" class="w-8 h-8 text-xl font-bold text-gold  flex items-center justify-center mr-6">+</button>
-            </div>
-
-            <div class="flex items-center justify-center ml-4 rounded-lg border-2 border-gold text-sm text-gold px-2 ">
-                @livewire('button', ['label'=> "Ajouter"])
-            </div>
+<article class="mx-6 my-2 flex border-2 border-gold rounded-lg items-center gap-2">
+   
+        <img src="{{ $bottle->image }}" alt="{{ $bottle->name }}" class="max-w-80">
+   
+        <div class="flex flex-col justify-end items-end p-4  sm:flex-row sm:justify-between sm:gap-4">
+            <h1 class="text-right font-bold font-roboto">{{ $bottle->name }}</h1>
+            <p class="text-xs mt-2 mb-2">{{ $bottle->description }}</p>
+            @livewire('button', ['label'=> "Ajouter", 'class' => 'text-sm flex items-center justify-center ml-4 rounded-lg px-3 py-2 bg-gold text-white'])
         </div>
 </article>

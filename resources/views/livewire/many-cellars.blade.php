53f876dd8002097775183f54aa156f08ff57f0af
maryline888 <maryline888@gmail.com>
2023-07-18 20:39:31 -0400

    modif design many cellars

--- resources/views/livewire/many-cellars.blade.php
+++ resources/views/livewire/many-cellars.blade.php
@@ -1,14 +1,15 @@
-<div style="margin-top: 2rem;">
-    <div class="grid grid-cols-3 gap-4 max-w-3xl mx-auto">
+<div class="mt-4 font-roboto">
+    <h1 class="text-center text-xl mb-6">Liste de vos celliers</h1>
+    <div class="flex flex-col gap-2 bg-pink ">
         @foreach ($cellars as $cellar)
-        <a href="{{ route('singleCellar', ['cellar_id' => $cellar->id]) }}" class="col">
-                <div class="card-body mb-4 text-center border-10 border-gray-300 rounded-lg" style="margin-right: 1rem;">
-                    <p class="card-title text-xl font-bold mb-2">ID: {{ $cellar->id }}</p>
-                    <p class="mb-4">Nom: {{ $cellar->name }}</p>
-                    <p class="mb-4">Créé le: {{ $cellar->created_at }}</p>
-                    <p class="mb-4">Mis à jour le: {{ $cellar->updated_at }}</p>
-                </div>
-            </a>
+        <article class="bg-gold mx-6 my-2 flex border-2 border-gold rounded-lg items-center gap-2 justify-center">
+            <a href="{{ route('singleCellar', ['cellar_id' => $cellar->id]) }}" class="">
+                    <div class="mb-4 text-center border-10 rounded-lg">
+                        <p class="hidden">ID: {{ $cellar->id }}</p>
+                        <p class="text-xl mt-4 text-white uppercase font-montserrat">{{ $cellar->name }}</p>
+                    </div>
+                </a>
+        </article>
         @endforeach
     </div>
 </div>

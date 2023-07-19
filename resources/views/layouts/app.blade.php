6a1f694a495dde42c6dcf79bb004b52023566d39
maryline888 <maryline888@gmail.com>
2023-07-18 21:09:17 -0400

    ajout titre appl

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Livewire Example</title>
+    <title>Vino</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @livewireStyles
 </head>
